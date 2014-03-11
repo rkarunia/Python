827107abff89a0bdcf16e46d47ef9334d932d777
Ron Karunia <rkarunia@yahoo.com>
2014-03-10 21:24:30 -0700

    Added function: kind(n,ranks)

--- Udacity/DesignComputerPrograms/Poker.py
+++ Udacity/DesignComputerPrograms/Poker.py
@@ -4,12 +4,7 @@
 # example: ['AC', '3D', '4S', 'KH'] should output [14, 13, 4, 3]
 def card_ranks(cards):
     "Return a list of the ranks, sorted with higher first."
-    rankdict = {'1':1,'2':2,'3':3,'4':4,'5':5,
-                '6':6,'7':7,'8':8,'9':9,
-                'T':10,'J':11,'Q':12,'K':13,'A':14}
-    ranks = [rankdict[r] for r,s in cards]
-    #alternative:
-    #ranks = ['--23456789TJQKA'.index(r) for r,s in cards]
+    ranks = ['--23456789TJQKA'.index(r) for r,s in cards]
     ranks.sort(reverse=True)
     return ranks
 
@@ -17,29 +12,42 @@ def card_ranks(cards):
 # Output: True if ranks form a straight hand, false otherwise
 def straight(ranks):
     "Return True if the ordered ranks form a 5-card straight."
-    # Your code here.
-    prev = ranks[0]+1
-    for rank in ranks:
-        if (prev - rank) is not 1:
-            return False
-        prev = rank
-    return True
-    # alternative:
-    # return ((max(ranks)-min(ranks)==4)&&len(set(ranks))==5)
+    return ((max(ranks)-min(ranks)==4) and len(set(ranks))==5)
 
 # Input: a list of cards in a hand, example: ['6C','9C','8C','7C','6C']
 # Output: True if the hand form a flush hand, false otherwise
 def flush(hand):
     "Return True if all the cards have the same suit."
-    # Your code here.
-    prev = hand[0][1]
-    for r,h in hand:
-        if h != prev:
-            return False
-        prev = h
-    return True
-    # alternative
-    # return len(set([s for r,s in hand]))==1   
+    return len(set([s for r,s in hand]))==1   
+
+# Input:
+# n -> the hand has exactly n of
+# ranks -> a list of card ranks
+# Output:
+# if the hand is "9D 9H 9S 9C 7D"
+# ranks -> [9,9,9,9,7]
+# if we call kind(4,ranks), it should return 9,
+# because the hand has 4 of 9
+def kind(n, ranks):
+    """Return the first rank that this hand has exactly n of.
+    Return None if there is no n-of-a-kind in the hand."""
+    if not ranks:
+        return None
+    count = {}
+    for rank in ranks:
+        if rank in count:
+            count[rank] = count[rank] + 1
+        else:
+            count[rank] = 1
+    for k,v in count.items():
+        if v == n:
+            return k
+    return None    
+    # alternative:
+    # for rank in ranks:
+    # if ranks.count(rank) == n:
+    # return rank
+    # return None
 
 def test():
     sf = "6C 7C 8C 9C TC".split() # Straight Flush
@@ -56,6 +64,13 @@ def test():
     assert straight([9, 8, 8, 6, 5]) == False
     assert flush(sf) == True
     assert flush(fk) == False
+
+    fkranks = card_ranks(fk)
+     
+    assert kind(4, fkranks) == 9
+    assert kind(3, fkranks) == None
+    assert kind(2, fkranks) == None
+    assert kind(1, fkranks) == 7
     
     print 'Test Passed'
     return True
